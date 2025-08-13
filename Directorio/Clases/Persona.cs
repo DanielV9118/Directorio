254a39fc0be6f817dd666cd34ca6dd1acf01f7f6
DanielV9118 <danielvalladares218@gmail.com>
2025-08-13 15:42:43 -0600

    Agregar archivos de proyecto.

---
+++ Directorio/Clases/Persona.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directorio.Clases
+{
+    public class Persona
+    {
+        private string nummeroid { get; set; }
+        private string nombres { get; set; }
+        public string apellidos { get; set; }
+
+        public string direccion { get; set; }
+
+        public int edad { get; set; }
+        public DateTime fechanacimiento { get; set; }
+        public string telefono { get; set; }
+        public string celular { get; set; }
+        public string email { get; set; }
+
+        public Persona()
+        {
+            this.nombres = "jose";
+
+        }
+        public Persona(string p_nombres)
+        {
+            this.nombres = p_nombres;
+        }
+        public string getNombre()
+        {
+            return this.nombres;
+
+        }
+        public void setNombre(string p_nombres)
+        {
+            this.nombres = p_nombres;
+                    }
+        public void setApellidos(string p_apellidos) {
+        {
+        this.apellidos = p_apellidos;   
+        }
+        }
+        public string getNombreCompleto()
+        {
+            return this.nombres + " " + this.apellidos;
+        }
+        public string getNombreCompleto(string p_nombres, string p_apellidos)
+        {
+            this.nombres = p_nombres;
+            this.apellidos = p_apellidos;
+            return this.nombres + " " + p_apellidos;
+        }
+    }
+    }
+
---
+++ Directorio/Program.cs
@@ -0,0 +1,31 @@
+﻿using Directorio.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            String nombreCompleto = string.Empty;
+
+
+
+            persona.setNombre("Pedro Antonio");
+            persona.setApellidos("Perez Perez");
+
+
+            nombreCompleto = persona.getNombreCompleto();
+            Console.WriteLine("Persona 1" + nombreCompleto);
+            nombreCompleto = persona.getNombreCompleto("Juan", "Lopez Lopez");  
+            Console.WriteLine("Persona 2" + nombreCompleto); 
+
+
+        }
+    }
+}
